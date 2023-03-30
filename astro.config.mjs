d1902eb1be25aa27e52b166d9007f5a24867f025
plukc <59031302+pluckpawz@users.noreply.github.com>
2023-03-30 14:21:11 +0200

    configurate astro

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://localhost:3000",
+  integrations: [mdx(), sitemap()]
+});
