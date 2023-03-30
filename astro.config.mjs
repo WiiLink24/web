9cd9ff0c1a9a34d9f2659936f1face4cb92e4d87
pluck <59031302+pluckpawz@users.noreply.github.com>
2023-03-30 15:20:53 +0200

    fix thing not working (idk how i fixed)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 
-import sitemap from "@astrojs/sitemap";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://localhost:3000",
-  integrations: [mdx(), sitemap()]
+	site: 'https://example.com',
+	integrations: [mdx(), sitemap()],
 });
---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'WiiLink24';
+export const SITE_DESCRIPTION = 'WiiLink is a revival service for the now-defunct Japanese exclusive Wii channels and media channels.';
