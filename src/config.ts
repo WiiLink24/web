1fb8f9b5d7b2cda760d0e0f0c9f73073a7525296
pluck <59031302+pluckpawz@users.noreply.github.com>
2023-03-30 14:41:40 +0200

    push wiilink bio

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 // WiiLink website global data
 
 export const SITE_TITLE = "WiiLink";
-export const SITE_DESCRIPTION = "This is a placeholder";
+export const SITE_DESCRIPTION = "WiiLink is a revival service for the now-defunct Japanese exclusive Wii channels and media channels.";
