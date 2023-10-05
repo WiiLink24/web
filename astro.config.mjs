afa3ea29b2e5afff55d7d53396a7d6b43f164e50
Alex <basurillodesgastorito@gmail.com>
2023-10-05 17:30:36 +0200

    Started work on news

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
+import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  integrations: [mdx()],
+  integrations: [astroI18next()]
 });
