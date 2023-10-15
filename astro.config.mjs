66376c25caa556eee3f24e26e97ded7a02827a92
Alex <basurillodesgastorito@gmail.com>
2023-10-15 19:10:06 +0200

    Updated news posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 import astroI18next from "astro-i18next";
+import markdownConfig from './markdown.config.ts'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
-  integrations: [astroI18next()]
+  markdown: markdownConfig,
+  integrations: [
+    mdx({
+      ...markdownConfig,
+      extendPlugins: false,
+    }),
+  ],
+  integrations: [astroI18next()],
+
 });
