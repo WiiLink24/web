b6ef6f4e67f793d9def791aa2bf911a186c27367
Alex <basurillodesgastorito@gmail.com>
2023-11-29 19:42:35 +0100

    Added wii site

--- public/js/osdetect.js
+++ public/js/osdetect.js
@@ -1,13 +1,13 @@
 function osdetect() {
 
-let userAgent = window.navigator.userAgent;
-
-let os = "Unknown OS";
-
-if (userAgent.indexOf("Win") != -1) os = "Windows";
-if (userAgent.indexOf("Mac") != -1) os = "MacOS";
-if (userAgent.indexOf("X11") != -1) os = "UNIX";
-if (userAgent.indexOf("Linux") != -1) os = "Linux";
-
-return os;
-}
+    let userAgent = window.navigator.userAgent;
+    let os = "Unknown OS";
+    
+    if (userAgent.indexOf("Win") != -1) os = "Windows";
+    if (userAgent.indexOf("Mac") != -1) os = "MacOS";
+    if (userAgent.indexOf("X11") != -1) os = "UNIX";
+    if (userAgent.indexOf("Linux") != -1) os = "Linux";
+    if (userAgent.indexOf("Wii") != -1) os = "Nintendo Wii";
+    
+    return os;
+    }
