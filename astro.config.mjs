d679aaeccbb9046a1c6f6391e1447234c008a7c3
Alex <basurillodesgastorito@gmail.com>
2024-03-22 07:42:20 +0100

    Removed WWFC banner.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,19 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 import astroI18next from "astro-i18next";
-import markdownConfig from './markdown.config.ts'
+import markdownConfig from './markdown.config.ts';
 import astroRemark from "@astrojs/markdown-remark";
 
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://wiilink24.com",
-  integrations: [mdx()],
+  integrations: [mdx(), partytown()],
   markdown: markdownConfig,
-  integrations: [
-    mdx({
-      ...markdownConfig,
-      extendPlugins: false,
-    }),
-  ],
-  integrations: [astroI18next()],
-
+  integrations: [mdx({
+    ...markdownConfig,
+    extendPlugins: false
+  })],
+  integrations: [astroI18next()]
 });
