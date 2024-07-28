5cff50642061a023ed16099e77e68fa18d25f97f
Alex <basurillodesgastorito@gmail.com>
2024-07-28 18:50:49 +0200

    Improved CSS optimization.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,14 @@ import mdx from "@astrojs/mdx";
 import astroI18next from "astro-i18next";
 import markdownConfig from './markdown.config.ts';
 import astroRemark from "@astrojs/markdown-remark";
-
 import partytown from "@astrojs/partytown";
+
+import purgecss from "astro-purgecss";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://wiilink24.com",
-  integrations: [mdx(), partytown()],
+  integrations: [mdx(), partytown(), purgecss()],
   markdown: markdownConfig,
   integrations: [mdx({
     ...markdownConfig,
