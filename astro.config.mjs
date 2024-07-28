639f2d12f4fd293fc01bd0c91027b1c473003ca9
Alex <basurillodesgastorito@gmail.com>
2024-07-28 19:08:40 +0200

    Added comopression

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,32 @@ import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 import astroI18next from "astro-i18next";
 import markdownConfig from './markdown.config.ts';
-import astroRemark from "@astrojs/markdown-remark";
 import partytown from "@astrojs/partytown";
-
 import purgecss from "astro-purgecss";
+import playformCompress from "@playform/compress";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://wiilink24.com",
-  integrations: [mdx(), partytown(), purgecss()],
-  markdown: markdownConfig,
-  integrations: [mdx({
-    ...markdownConfig,
-    extendPlugins: false
-  })],
-  integrations: [astroI18next()]
+  integrations: [
+    mdx({
+      ...markdownConfig,
+      extendPlugins: false
+    }),
+    partytown(),
+    purgecss(),
+    playformCompress(),
+    astroI18next(),
+    (await import("@playform/compress")).default({
+      CSS: true,
+      HTML: {
+        "html-minifier-terser": {
+          removeAttributeQuotes: true
+        }
+      },
+      Image: true,
+      JavaScript: true,
+      SVG: true
+    })
+  ],
+  markdown: markdownConfig
 });
