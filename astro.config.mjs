54fd14aa74f1dffc260447b9d0c4684f3166a17e
Alex <basurillodesgastorito@gmail.com>
2024-12-27 17:15:24 +0100

    Major guide revamp!

--- astro.config.mjs
+++ astro.config.mjs
@@ -25,7 +25,7 @@ export default defineConfig({
     SVG: true
   }), icon({
     include: {
-      "lucide": ["info", "message-circle", "globe", "film", "utensils", "user", "download", "house", "mail-open", "wifi", "newspaper", "book", "arrow-right", "circle-alert", "circle-help", "server", "package", "package-open", "users", "chart-no-axes-column", "tag", "id-card", "stamp", "bookmark", "disc", "gamepad-2", "shield", "feather", "moon", "sun", "arrow-up-right", "menu", "earth", "shopping-basket", "smile", "zap", "monitor", "cat", "palette", "chart-pie", "film", "video", "mic", "tv", "circle", "truck", "camera", "bot", "leaf", "chevron-down", "presentation", "umbrella", "mail", "rss", "chevron-right", "brush"],
+      "lucide": ["info", "message-circle", "globe", "film", "utensils", "user", "download", "house", "mail-open", "wifi", "newspaper", "book", "arrow-right", "circle-alert", "circle-help", "server", "package", "package-open", "users", "chart-no-axes-column", "tag", "id-card", "stamp", "bookmark", "disc", "gamepad-2", "shield", "feather", "moon", "sun", "arrow-up-right", "menu", "earth", "shopping-basket", "smile", "zap", "monitor", "cat", "palette", "chart-pie", "film", "video", "mic", "tv", "circle", "truck", "camera", "bot", "leaf", "chevron-down", "presentation", "umbrella", "mail", "rss","chevron-left", "chevron-right", "brush", "clock"],
       "simple-icons": ["twitter", "bluesky", "github", "instagram", "youtube", "discord", "microsoft", "apple", "linux", "android"]
     }
   })],
