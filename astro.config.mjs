6189b8241daa674e0e4dfc9cf80452142fadde72
Alex <basurillodesgastorito@gmail.com>
2025-01-02 19:24:25 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,17 +13,7 @@ export default defineConfig({
   integrations: [mdx({
     ...markdownConfig,
     extendPlugins: false
-  }), partytown(), purgecss(), astroI18next(), (await import("@playform/compress")).default({
-    CSS: true,
-    HTML: {
-      "html-minifier-terser": {
-        removeAttributeQuotes: true
-      }
-    },
-    Image: true,
-    JavaScript: true,
-    SVG: true
-  }), icon({
+  }), partytown(), astroI18next(), icon({
     include: {
       "lucide": ["info", "message-circle", "globe", "film", "utensils", "user", "download", "house", "mail-open", "wifi", "newspaper", "book", "arrow-right", "circle-alert", "circle-help", "server", "package", "package-open", "users", "chart-no-axes-column", "tag", "id-card", "stamp", "bookmark", "disc", "gamepad-2", "shield", "feather", "moon", "sun", "arrow-up-right", "menu", "earth", "shopping-basket", "smile", "zap", "monitor", "cat", "palette", "chart-pie", "film", "video", "mic", "tv", "circle", "truck", "camera", "bot", "leaf", "chevron-down", "presentation", "umbrella", "mail", "rss","chevron-left", "chevron-right", "brush", "clock"],
       "simple-icons": ["twitter", "bluesky", "github", "instagram", "youtube", "discord", "microsoft", "apple", "linux", "android"]
